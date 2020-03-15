a26676a7a8dae5a293d131314f98738e0294bc8c
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-03-15 18:45:45 +0100

    added antd + emotion

--- frameworks/react/mapbox_test/src/App.tsx
+++ frameworks/react/mapbox_test/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Button, DatePicker, version } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test paragraph.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "antd/dist/antd.css";
+
+import styled from '@emotion/styled'
+
+const StyledDiv = styled.div`
+  margin-left: 100px;
+`
+
+class App extends Component {
+  render() {
+    return (
+      <StyledDiv>
+        <h1>antd version: {version}</h1>
+        <DatePicker />
+        <Button type="primary" style={{ marginLeft: 8 }}>
+          Primary Button
+        </Button>
+      </StyledDiv>
+    );
+  }
 }
 
 export default App;
