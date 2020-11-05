5b6391088b4347d2d6852ea215218ee7e9df2238
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-11-05 18:33:47 +0100

    init octonotes from cra

--- octokit/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-// https://javascriptplayground.com/typescript-eslint/
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended'],
-  rules:  {
-    "no-unexpected-multiline": "error",
-  }
-}
