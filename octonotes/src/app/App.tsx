18e092fa78b2392d7d6cdb4ccffa9eb77098367e
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-11-05 19:16:22 +0100

    minimal setup with antd and octokit

---
+++ octonotes/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+import * as octokit from "./octokit"
+
+// octokit.getData()
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
